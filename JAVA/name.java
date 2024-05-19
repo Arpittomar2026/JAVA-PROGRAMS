bf742c841a45f46b0a11db9447f5476e6b7baad7
Arpit tomar <158043482+Arpittomar2026@users.noreply.github.com>
2024-05-19 21:41:26 +0530

    Add files via upload

---
+++ JAVA/name.java
@@ -0,0 +1,16 @@
+package JAVA;
+
+public class name {
+    public static void  funcname(char n){
+        char name;
+        System.out.println("Arpit");
+        for(i=1; i<n; i++){
+            
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Arpit ");
+    }
+    
+}
